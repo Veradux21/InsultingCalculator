8ced2c7656830857bc1c2e6b3b8f8fdc6b970042
Veradux21 <Veradux4@protonmail.com>
2022-07-13 15:37:20 -0400

    Add Insults, Make Calculator MVP feature complete
    Calculator is now able to successfully:
        - Do basic operations (Add, subtract, multiply and divide).
        - Trigger operation with = button.
        - Clear all values with C button.
        - Display values as they are entered, including a parsed operator values
        - Returns accurate result value to a single decimal point.
    
    Will need visual polish but the Calculator itself is complete
    
    TODO complete Insult function to add new functions on =

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+const insultsList = [
+  "I created an algorithm to calculate your moms weight. Unfortunately I got a stackoverflow error.",
+  "The bar was the cement, and you brought a shovel.",
+  "I expected nothing, but I'm still disappointed.",
+  "You are the reason we have warning labels.",
+  "I see you are still regularly eating Tide pods.",
+  "Aww, bless your heart.",
+  "Did you have to break out the iron to get your brain that smooth?",
+  "You are a sad strange little person, and you have my pity.",
+  "Well, I'll tell you something that should be of vital interest to you. That you are a NITWIT!",
+  "You are about one bit short of a byte.",
+  "Every year on your birthday your parents go to the zoo and throw stones at the stork.",
+  "You do math like you got lobotomized by a scented marker after shoving it up your nose too deep.",
+  "Your math skills are rivaled by that of my bathroom sink.",
+  "I would rather face 5 battalions of reinforced panzers than have to watch you do math.",
+  "The only thing weaker than your math skills is your hairline",
+  "You're the kind of person to bring out a guitar at a party and play a traumatizing acoustic rendition of Wonderwall",
+  "Jellyfish have survived for 600 million years without a brain. At least there's some hope for you.",
+  "Incredible... when you move your head it sounds like maracas."
+]
+
 const sum = function(...arg) {
     return arg.reduce((total, args) => total + args, 0)
   };
@@ -48,27 +69,43 @@ const updateText = (...objects) => {
     if (operatorVar == 0){
       display.innerText = `${num0Var}`
     } else if (num1Var == 0){
-      display.innerText = `${num0Var} ${operatorVar}`
+      display.innerText = `${num0Var} ${operatorVisual}`
     } else {
-      display.innerText = `${num0Var} ${operatorVar} ${num1Var}`
+      display.innerText = `${num0Var} ${operatorVisual} ${num1Var}`
     };
 }
 
 const manageValues = (e) => {
-  if (e.target.dataset.operator > 0){
+  if (e.target.dataset.operator == "equals"){
+    num0Var = operate(operatorVar, num0Var, num1Var);
+    operatorVar = 0;
+    num1Var = 0;
+    updateText();
+    insult();
+    return
+  } else if (e.target.dataset.operator == "clear"){
+    operatorVar = 0;
+    num0Var = 0;
+    num1Var = 0;
+  } else if (e.target.dataset.operator > 0){
     operatorVar = e.target.dataset.operator;
     console.log("Set operator to ", e.target.dataset.operator);
+    operatorVisual = e.target.innerText;
   } else if (operatorVar == 0) {
     num0Var = parseInt(num0Var + e.target.innerText)
     console.log(num0Var)
+  } else {
+    num1Var = parseInt(num1Var + e.target.innerText)
+    console.log(num1Var)
   };
-  updateText(num0Var,operatorVar,num1Var);
+  updateText(num0Var,operatorVisual,num1Var);
 }
 
 const insult = () => {
 
 }
 
+let operatorVisual;
 let operatorVar = 0;
 let num0Var = 0;
 let num1Var = 0;
