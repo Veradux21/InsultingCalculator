ee69990e5036f6aefbd2304cec62f41aa08ca5d3
Veradux21 <Veradux4@protonmail.com>
2022-07-15 21:25:29 -0400

    Add looping Calc, Make Clear, Numbers, & Operators Function with Array
    Lots in this commit...
    Added calculator that verifies that user has submitted at least Numbers == 1 + Operators
    Added loop to run calculations of multiple operations
    added logic to add numbers in the following use cases:
        1. if last entry was a number, pop, combine with new value and push to add the new entry as part of the existing number
        2. if last entry was an operator, add as new number to the array
    
    added logic to add Operators
        1. if last was a number, add new operator
        2. if last was operator, replace old operator.
    
    Also debugged update text, split off precalculation updates into its own function.
    BUG: Was unable to debug where an operator is added prior to number
    
    Project has met all MVP requirements.

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ This is a proof of concept for my ability to create a calculator webapp built in
 The target is to allow a user to do basic calculations (add, subtract, multiply, divide) with multiple calculations.
 
 As an added bonus, I've also decided to make the UI deliver an insult every time the user runs a calculation.
+
+## Final Result
+I have been able to create the calculator described above with the caveat that adding an operator prior to any numerals fails to display the operator between entering the first and second numeral.
--- script.js
+++ script.js
@@ -34,7 +34,7 @@ const zeroInsultsList = [
 const insult = () => {
   const insulter = document.querySelector('.insulter')
   let insult = document.createElement('p');
-  if (num0Var == Infinity) {
+  if (displayString == Infinity) {
    insult.textContent = zeroInsultsList[Math.floor(Math.random() * zeroInsultsList.length)];
  } else {
    insult.textContent = insultsList[Math.floor(Math.random() * insultsList.length)];
@@ -45,7 +45,6 @@ const insult = () => {
 const sum = function(...arg) {
     return arg.reduce((total, args) => total + args, 0)
   };
-  
 
 const subtract = function(...arg) {
   let tractResult = arg[0]
@@ -87,72 +86,93 @@ const operate = (operator, ...numbers) => {
     return result;
 };
 
+let calculation;
 const manageValuesArray = (e) => {
   //Identify whether button is operator, numeral, other
-  if (e.target.dataset.operator == "equals"){
-  } else if (e.target.dataset.operator == "clear"){ // Clear button empties all arrays
+  if (e.target.dataset.operator == "equals"){ // *EQUALS: If pressed button is equals, calculate.
+    if (operatorArray.length  >= numArray.length){
+      insult();
+      return;
+    } else { 
+      calculation = operate(operatorArray[0], numArray[0], numArray[1])
+      if(operatorArray.length > 1){for (let i = 2; i < numArray.length; i++){
+        calculation = operate(operatorArray[i-1], calculation, numArray[i])
+        console.log("calc loop")
+      }}
+    }
+    updateText(calculation);
+    insult();
+  } else if (e.target.dataset.operator == "clear"){ // *CLEAR: If pressed button is clear, empties all arrays
     operatorArray = [];
     operatorVisArray = [];
-    numArray = [0];
+    numArray = [];
+    displayString = "";
+    updateText(displayString);
     console.log("Clear");
-  } else if (e.target.dataset.operator > 0){
-    operatorArray.push(parseInt(e.target.dataset.operator))
-    operatorVisArray.push(`${e.target.innerText}`)
-    console.log("operator ", operatorVisArray, " + ", operatorArray)
-  } else {
-    numArray.push(e.target.innerText)
-    console.log("Add number: ", numArray)
+    return;
+  } else if (e.target.dataset.operator > 0){ // *OPERATOR TREE: If pressed button is an operator...
+    if (numArray.length > operatorArray.length){  // If the last entered value was a number then push operator into array
+      operatorArray.push(parseInt(e.target.dataset.operator))
+      operatorVisArray.push(`${e.target.innerText}`)
+      console.log("operator ", operatorVisArray, " + ", operatorArray);
+    } else if (numArray.length == operatorArray.length){ // If the last entered value was an operator, replace old operator
+      operatorArray.pop();
+      operatorVisArray.pop();
+      operatorArray.push(parseInt(e.target.dataset.operator));
+      operatorVisArray.push(`${e.target.innerText}`);
+      console.log("operator ", operatorVisArray, " + ", operatorArray);
+    } else {
+      console.log("Operator Tree, something is wrong:", operatorArray, numArray)
+      return;
+    } 
+    preCalcUpdate();
+  } else { // *NUMBER TREE: If pressed button is not an operator
+    if (numArray.length > operatorArray.length){ // If last entered value was a number, then add button as string to last value
+      let handler = numArray.pop();
+      handler = parseInt(handler + e.target.innerText);
+      numArray.push(handler);
+      console.log("Expand String: ", numArray)
+    } else if (numArray.length == operatorArray.length){ // If last entered value was an operator, then add new number
+      numArray.push(parseInt(e.target.innerText))
+      console.log("Add number: ", numArray)
+    } else {
+      numArray.push(parseInt(e.target.innerText))
+      console.log("operator>num: ", numArray)
+    }
+    preCalcUpdate();
   }
-  // Managing Display Value
 
-  updateText();
 }
 
-const updateText = () => {
-  displayString = numArray[0].toString();
-  for (let i = 1; i < numArray.length; i++) {
-    displayString = `${displayString}`+ " " + operatorVisArray[i - 1] + " ";
-    displayString = displayString + numArray[i].toString();
-  }  
+const preCalcUpdate = () => { 
+  displayString = "";
+  if (numArray[0] !== undefined && operatorArray[0] !== undefined){
+    displayString = numArray[0] /* + " " + operatorVisArray[0] */; //If I add the commented code then I get 2 operator [0]s and it gets all sorts of messed up.
+    for (let i = 1; i < numArray.length; i++) {
+      displayString = `${displayString}`+ " " + operatorVisArray[i-1] + " ";
+      displayString = displayString + numArray[i].toString();
+    }
+    console.log("num and operator defined")
+  } else if (numArray[0] !== undefined){
+    displayString = numArray[0].toString()
+    console.log("num defined, operator undefined")
+  }
+  else if (numArray[0] == undefined && operatorArray !== undefined) { // TODO: need to figure out how to allow operator first to display and then 
+    displayString = operatorVisArray[0];
+    console.log("Operator before num")
+  } else { updateText(""); console.log("Undefined catch") };  // Show "" instead of 0 or undefined
+updateText(displayString);
+}
+
+const updateText = (e) => {
   const display = document.querySelector('#display');
-  display.innerText = `${displayString}`
+  display.innerText = `${e}`
   console.log("Update Text")
 }
 
-const manageValues = (e) => {
-  if (e.target.dataset.operator == "equals"){
-    num0Var = operate(operatorVar, num0Var, num1Var);
-    operatorVar = 0;
-    num1Var = 0;
-    updateText();
-    insult();
-    return
-  } else if (e.target.dataset.operator == "clear"){
-    operatorVar = 0;
-    num0Var = 0;
-    num1Var = 0;
-  } else if (e.target.dataset.operator > 0){
-    operatorVar = e.target.dataset.operator;
-    console.log("Set operator to ", e.target.dataset.operator);
-    operatorVisual = e.target.innerText;
-  } else if (operatorVar == 0) {
-    num0Var = parseInt(num0Var + e.target.innerText)
-    console.log(num0Var)
-  } else {
-    num1Var = parseInt(num1Var + e.target.innerText)
-    console.log(num1Var)
-  };
-  updateText(num0Var,operatorVisual,num1Var);
+const test = () => {
+  console.log(numArray, operatorArray, displayString)
 }
-
-
-
-
-
-let operatorVisual;
-let operatorVar = 0;
-let num0Var = 0;
-let num1Var = 0;
 
 let displayString;
 let numArray = [];
